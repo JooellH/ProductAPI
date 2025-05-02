64cb8ccf95d6e032fd4d346630fe4d5a467fa8f4
joel holz <joelholz9@gmail.com>
2025-05-02 20:48:57 -0300

    Archivos agregados

--- ProductAPI/obj/Debug/net6.0/ProductAPI.AssemblyInfo.cs
+++ ProductAPI/obj/Debug/net6.0/ProductAPI.AssemblyInfo.cs
@@ -11,11 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("f410feff-543f-41ca-a069-a4cb4eda8314")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc75992341bd6dfc6db8e1bd4f050e920bc1c18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3a9da14b56a59db07500f696797ea9af7c316e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
